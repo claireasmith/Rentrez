9a7c9b447cdcf141183719e90a8c2627714d390e
Claire Smith <claire.hass.smith@gmail.com>
2019-01-24 16:06:28 -0500

    Download.R edits

--- Download.R
+++ Download.R
@@ -1,4 +1,4 @@
-library(rentrez) # load rentrez into session
+library(rentrez) # load rentrez package
 
 ncbi_ids <- c("HQ433692.1","HQ433694.1","HQ433691.1") # make a vector of unique
 # identifiers for the NCBI files we want
@@ -7,19 +7,22 @@ Bburg<-entrez_fetch(db = "nuccore", id = ncbi_ids, rettype = "fasta") # pass the
 # unique identifiers to the NCBI nucleotide database (nuccore) and get the data
 # files in FASTA format
 
+str(Bburg)
+print(Bburg)
+
 # Get rid of whitespace and split the string from NCBI into a vector, each component
 # containing a different sequence
-test <- gsub("\\n*","",Bburg)
-test <- strsplit(test, ">")
-test <- unlist(test)
-test <- test[grepl("\\w+", test)] # keep only non-empty vectors
+Bburg2 <- gsub("\\n*","",Bburg)
+Bburg2 <- strsplit(Bburg2, ">")
+Bburg2 <- unlist(Bburg2)
+Bburg2 <- Bburg2[grepl("\\w+", Bburg2)] # keep only non-empty vectors
 
 # Loop through each sequence in the vector, take out the id and make it the
 # sequence's name in the vector, remove all other info but the sequence
-bseqs <- c()
-for (i in 1:length(test)){
-  bseqs[i] <- gsub("HQ.*sequence", "", test[i])
-  names(bseqs)[i] <- gsub(" Borrelia.*", "", test[i])
+Bseqs <- c()
+for (i in 1:length(Bburg2)){
+  Bseqs[i] <- gsub("HQ.*sequence", "", Bburg2[i])
+  names(Bseqs)[i] <- gsub(" Borrelia.*", "", Bburg2[i])
 }
 
-write.csv(bseqs, file="Sequences.csv")
+write.csv(Bseqs, file="Sequences.csv")
